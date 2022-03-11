d34a154f7cb70fac8b70e67719b9ecee9080ab51
Stanislav Kuznietsov <bisu.stats@gmail.com>
2022-03-11 19:50:39 +0200

    Initial

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+     <img src="https://images.cults3d.com/qDSS79biyEbudtXRDSUERfUiQi4=/https://files.cults3d.com/uploaders/15269097/illustration-file/729d6985-3176-4298-abbd-0beb5703390f/WhatsApp%20Image%202020-05-15%20at%2019.33.48.jpeg"/>
+    </header>
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+    <div>
+       <img src="https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&w=1000&q=80" alt=""/> 
+    </div>
+     <div>
+       ava+Description
+     </div>
+     <div>
+       My posts
+       <div>
+         New post
+       </div>
+       <div> 
+         <div>Post #1</div>
+         <div>Post #2</div>
+       </div>
+     </div>
+   </div>
+}
+export default Profile;
